8ba341e8cdc48e276c83a557283c4222590a5695
Vincent Weevers <mail@vincentweevers.nl>
2016-09-25 13:57:53 +0200

    test list

--- test/index.js
+++ test/index.js
@@ -257,6 +257,54 @@ test('inspect', function (t) {
   })
 })
 
+test('list', function (t) {
+  t.plan(4)
+
+  const s1 = spy({ result: fixture('list.txt') })
+
+  t.is(Machine.prototype.list, undefined, 'static-only method')
+
+  Machine.list((err, result) => {
+    t.ifError(err, 'no inspect error')
+    t.same(result, [
+      { active: '*'
+      , activeHost: true
+      , activeSwarm: false
+      , dockerVersion: 'v1.11.2'
+      , driverName: 'virtualbox'
+      , error: null
+      , name: 'default'
+      , responseTime: 290
+      , state: 'running'
+      , swarm: null
+      , url: 'tcp://192.168.99.100:2376' },
+      { active: '-'
+      , activeHost: false
+      , activeSwarm: false
+      , dockerVersion: 'v1.11.0'
+      , driverName: 'virtualbox'
+      , error: null
+      , name: 'host-1'
+      , responseTime: 327
+      , state: 'stopped'
+      , swarm: null
+      , url: 'tcp://192.168.99.101:2376' },
+      { active: '* (swarm)'
+      , activeHost: true
+      , activeSwarm: true
+      , dockerVersion: 'v1.11.2'
+      , driverName: 'virtualbox'
+      , error: null
+      , name: 'host-2'
+      , responseTime: 405
+      , state: 'running'
+      , swarm: null
+      , url: 'tcp://192.168.99.102:2376' }
+    ])
+    t.same(s1.args, ['ls', '-f', fixture('template.txt')])
+  })
+})
+
 function spy(state) {
   spies.push(state)
   return state
